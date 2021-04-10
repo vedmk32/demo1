911258bce3f5e21b4275dbdbbe2b5f072a14dbcb
vedmk32 <vedmk32@gmail.com>
2021-04-10 17:53:15 +0530

    class modified

--- demoProject1/src/demoProject1/Welcome.java
+++ demoProject1/src/demoProject1/Welcome.java
@@ -4,7 +4,7 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome!!");
+		System.out.println("Welcome!");
 
 	}
 
